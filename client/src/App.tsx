43913e5076859795a90196d7125c1d4bbb764ec8
MarcusAlien <buscabodega@gmail.com>
2025-09-12 20:10:44 -0600

    Netlify fix
    fix

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,7 @@
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/UI/toaster";
+import { Toaster } from "@/components/UI/toaster.tsx";
 import { TooltipProvider } from "@/components/UI/tooltip";
 import Home from "@/pages/Home";
 import Collections from "@/pages/Collections";
